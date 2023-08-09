a4130e643c3548ed1635f91dd787040b5f7c375a
lilygarfinkel <lilygarfinkel8@gmail.com>
2023-08-09 12:54:50 -0400

    pattern pix

--- src/Pattern.jsx
+++ src/Pattern.jsx
@@ -79,6 +79,7 @@ function Pattern(props) {
     document.onkeydown = function (e) {
         console.log(e.key);
         let thisPix = parseInt(highlightedPix);
+        hrow = highlightedRow.substring(3,4);
         let thisRow = parseInt(highlightedRow);
         console.log(thisPix, thisRow);
         switch (e.key) {
